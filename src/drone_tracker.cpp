68a12bad6cca62211ae1cb8400fd86def9762540
Naveen Tripathi <naveen.tripathi1010@gmail.com>
2017-10-21 13:10:49 +0530

    Added camera feed reciever in offboard mode

---
+++ src/drone_tracker.cpp
@@ -0,0 +1,113 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/tracking.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+//using namespace ros;
+
+void drone_tracker(const sensor_msgs::ImageConstPtr& drone_feed){
+    /*
+     * Need to do following
+     * 1. get image msg to opencv image
+     * 2. apply the tracker
+     * 3. show the feed.
+    */
+    Mat feed;
+    feed = cv_bridge::toCvCopy(drone_feed,sensor_msgs::image_encodings::BGR8)->image;
+    imshow("Feed", feed);
+    if(waitKey(100)==27)
+	    return ;
+
+}
+
+mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+	current_state = *msg;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc,argv,"drone_tracker");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+
+    //Publisher and Subscriber for drone camera
+    ros::Publisher tracked = nh.advertise<sensor_msgs::Image>("kcf_track", 10);
+    ros::Subscriber cam_feed = nh.subscribe<sensor_msgs::Image>("drone/camera/image_raw", 10, drone_tracker);
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
+
+    // wait for FCU connection
+    while(ros::ok() && current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 2;
+
+    pose.pose.orientation.x = 0;
+    pose.pose.orientation.y = 0;
+    pose.pose.orientation.z = 0;
+    pose.pose.orientation.w = 0;
+
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+//    pose.pose.position.x = 5;
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+
+    while(ros::ok()){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+        local_pos_pub.publish(pose);
+	
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+	
+}
+
